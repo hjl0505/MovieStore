9eb48e4d85135dc1ede643cd513779b75ed9dc7f
Chris Knakal <cknakal@gmail.com>
2016-03-12 12:03:27 -0800

    Updating Classic.cpp

--- Movie/Classic.cpp
+++ Movie/Classic.cpp
@@ -3,7 +3,7 @@
 // CSS 343 HW 4: Movie Store
 // 2/29/2016
 // ----------------------------------------------------------------------------
-// Sub class of Movie, classic
+// Sub class of Movie, Classic
 // ----------------------------------------------------------------------------
 // Inherits methods from the Movie object
 // Contains overriden display method
@@ -19,8 +19,8 @@
 //Default constructor
 Classic::Classic()
 {
-  actorList = NULL;
   monthReleased = 0;
+  actor = "";
 }
 
 //Overload constructor
@@ -30,11 +30,8 @@ Classic::Classic(char genre, char mediaType, string title, string director,
    int stock, int yearReleased, int monthReleased, string actor)
     : Movie(genre, mediaType, title, director, stock, yearReleased)
 {
-  actorList = new actorNode;
-  actorList -> actor = actor;
-  actorList -> stock = stock;
-  
   this->monthReleased = monthReleased;
+  this->actor = actor;
 }
 
 //Destructor
@@ -50,6 +47,7 @@ Classic::~Classic()
 //////////////////////////////////////////////////
 
 //Display
+//Displays Movie::display() and
 //Displays the monthReleased and actor
 void Classic::display() const
 {
@@ -61,47 +59,38 @@ void Classic::display() const
     cout << "Stock: " << stock << endl;
 
     cout << "Month Released: " << monthReleased << endl;
-    //cout << "Actor: " << actor << endl;
+    cout << "Actor: " << actor << endl;
 }
 
+//Get Movie Info
+//Returns a string of the movie's data variables
 string Classic::getMovieInfo() const
 {
     string s;
     //s = title + " " + director + " " + to_string(yearReleased) + " " + genre + " " + mediaType + " " + to_string(stock) + " " + to_string(monthReleased) + " " + actor;
     return s;
-    
+
 }
 
-//Get month
-//Returns the monthReleased
+//Get Month
+//Returns an int of the monthReleased
 int Classic::getMonth() const
 {
   return monthReleased;
 }
 
 //Get actor
-//Returns the actor
+//Returns a string of the actors associated with the Classic movie
 string Classic::getActor() const
 {
-  return "";
-}
-
-void Classic::addActor(string actor, int stock) 
-{
-	actorNode* temp = new actorNode;
-	temp -> actor = actor;
-	temp -> stock = stock;
-	temp -> next = NULL;
-	
-	if(actorList == NULL)
-	{
-		actorList = temp;
-	}
-	else
-	{
-		temp -> next = actorList;
-		actorList = temp;
-	}
+    returnString = "";
+    actorNode temp = actorList;
+    while (temp != NULL)
+    {
+        returnString += temp->actor;
+        temp = temp->next;
+    }
+    return returnString;
 }
 
 
@@ -119,7 +108,7 @@ void Classic::addActor(string actor, int stock)
 //considered to be equivalent
 bool Classic::operator == (const Movie& otherMovie) const
 {
-  if (monthReleased == otherMovie.getMonth() && yearReleased == otherMovie.getYearReleased() && actorExists(otherMovie.getActor()))
+  if (monthReleased == otherMovie.getMonth() && yearReleased == otherMovie.getYearReleased())
   {
     return true;
   }
@@ -127,12 +116,6 @@ bool Classic::operator == (const Movie& otherMovie) const
   {
     return false;
   }
-}
-
-
-bool Classic::actorExists(string actorName) const
-{
-	return true;
 }
 
 
@@ -166,7 +149,7 @@ bool Classic::operator < (const Classic& otherMovie) const
 Classic& Classic::operator = (const Classic& source)
 {
     monthReleased = source.getMonth();
-    //actor = source.getActor();
+    actor = source.getActor();
     return *this;
 }
 
